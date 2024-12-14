ffbd5274dc13bf7799e69331a843f477850b2051
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-12-13 22:44:20 -0800

    remove drawing funcs and string.concat

--- lua/autorun/!!!(_clib_init.lua
+++ lua/autorun/!!!(_clib_init.lua
@@ -5,7 +5,6 @@ AddCSLuaFile("clib/meta/sh_vehicles.lua")
 AddCSLuaFile("clib/util/sh_util.lua")
 
 if SERVER then
-    AddCSLuaFile("clib/panels/cl_drawing.lua")
     AddCSLuaFile("clib/panels/cl_meta.lua")
 end
 
@@ -14,8 +13,5 @@ include("clib/meta/sh_vehicles.lua")
 include("clib/util/sh_util.lua")
 
 if CLIENT then
-    include("clib/panels/cl_drawing.lua")
     include("clib/panels/cl_meta.lua")
-else
-    include("clib/util/sv_playerload.lua")
 end
--- lua/clib/util/sh_util.lua
+++ lua/clib/util/sh_util.lua
@@ -1,3 +1,20 @@
+--[[
+    gm_playerload - Copyright Notice
+    © 2024 CFC Servers - All rights reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 local string = string
 local math = math
 local emptyString = ""
@@ -13,15 +30,29 @@ function string.IsValid(str)
     return str != nil and str != emptyString
 end
 
-local tableConcat = table.concat
+local loadQueue = {}
+local conflictChecked = false
 
----------------------------------
--- string.concat
----------------------------------
--- Desc:        Concatenates the provided strings together.
--- State:       Shared
--- Arg(s):      String(s) - The strings to concatenates together.
--- Returns:     String - Concatenated string.
-function string.concat(...)
-    return tableConcat({...}, emptyString)
-end
+hook.Add("PlayerInitialSpawn", "CLib.PlayerLoad.Setup", function(ply)
+    loadQueue[ply] = true
+
+    -- Bit hacky, but if the players server has gm_playerload too then the collision needs to be fixed.
+    if !conflictChecked then
+        hook.Remove("SetupMove", "GM_FullLoadTrigger")
+
+        conflictChecked = true
+    end
+end)
+
+hook.Add("SetupMove", "CLib.PlayerLoad.Trigger", function(ply, mv, cmd)
+    if !loadQueue[ply] or cmd:IsForced() then
+        return
+    end
+
+    loadQueue[ply] = nil
+
+    -- Wrapped in a ProtectedCall so one bad hook.Add doesn't kill the whole script.
+    ProtectedCall(function()
+        hook.Run("PlayerFullLoad", ply)
+    end)
+end)
