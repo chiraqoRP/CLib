0276c71378ae7cb9c9f4ed3a17e1031b3d8397de
afxnatic <174385871+afxnatic@users.noreply.github.com>
2024-12-19 12:19:33 -0800

    Removed SizeToContentsY override

--- lua/autorun/!!!(_clib_init.lua
+++ lua/autorun/!!!(_clib_init.lua
@@ -4,14 +4,6 @@ CLib = CLib or {}
 AddCSLuaFile("clib/meta/sh_vehicles.lua")
 AddCSLuaFile("clib/util/sh_util.lua")
 
-if SERVER then
-    AddCSLuaFile("clib/panels/cl_meta.lua")
-end
-
 -- Includes
 include("clib/meta/sh_vehicles.lua")
 include("clib/util/sh_util.lua")
-
-if CLIENT then
-    include("clib/panels/cl_meta.lua")
-end
